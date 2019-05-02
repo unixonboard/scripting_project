0a0c0d8ebafa3f981f3cd9ef90e69b7f948016cd
unixonboard <unixonboard@gmail.com>
2019-05-02 11:17:15 -0700

    bash_shell_project added

---
+++ bash_shell_project/loop_though_file.sh
@@ -0,0 +1,7 @@
+#Write a code to loop through file 
+
+#1. print each 2nd column in the file  
+#2. Count the number of occurrence of word AND in the 2nd column of each line.
+
+#records in file are comma separated 
+#e.g a , b , c , d
